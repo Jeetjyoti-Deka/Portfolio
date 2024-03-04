9300e75399bb7cc66d5ee680474aa3c70fd46826
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-03-04 21:34:06 +0530

    Make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
       }}
       className="fixed top-0 left-0 w-screen h-20 px-6 bg-white z-50 flex items-center justify-between border-b-2 border-solid shadow-md"
     >
-      <h1 className="poppins-medium text-2xl">Jeetjyoti Deka</h1>
-      <div className="flex items-center gap-x-6">
+      <h1 className="poppins-medium text-xl sm:text-2xl">Jeetjyoti Deka</h1>
+      <div className="sm:flex items-center gap-x-6 hidden">
         {NAV_LINKS.map((link) => (
           <a
             className="poppins-medium text-lg"
@@ -29,6 +29,13 @@ const Navbar = () => {
             {link.label}
           </a>
         ))}
+      </div>
+      <div className="sm:hidden block w-6 h-6">
+        <img
+          src="src/assets/menu.svg"
+          alt="menu"
+          className="w-full h-full object-contain"
+        />
       </div>
     </motion.nav>
   );
